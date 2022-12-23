98f58e652a6f2589ff0121db263b3c6fa155622f
Radochin Artem <102265790+RadochinArtem@users.noreply.github.com>
2022-12-23 21:01:50 +0300

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld beanHWFirst = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanHWSecond = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCatFirst = (Cat) applicationContext.getBean("cat");
+        Cat beanCatSecond = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanHWFirst.equals(beanHWSecond));
+        System.out.println(beanCatFirst.equals(beanCatSecond));
+    }
+}
